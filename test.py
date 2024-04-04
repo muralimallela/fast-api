5e8a1c7aebc0960cb24837511c831c53c686826d
Murali Krishna <mmallela9@gmail.com>
2024-04-04 07:34:18 +0530

    adding alembic

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-import os
+num = int(input("Enter a number: "))
 
-print(os.getenv("Path"))
+while num > 0:
+    n = num % 10
+    is_prime = True
+    if n < 2:
+        is_prime = False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                is_prime = False
+                break
+    if is_prime:
+        print(n)
+    num = int(num / 10)
+
+
